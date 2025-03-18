aa15b27bbd3f4db0aae897b1710fc691a3469482
Curtis <curtis@gmail.com>
2025-03-18 10:23:20 +0300

    Python Basics

---
+++ Strings.py
@@ -0,0 +1,14 @@
+message = "hello , how are you doing?"
+print(message)
+#Functions
+print(message.capitalize())
+print(message.upper())
+print(message.lower())
+print(message.islower())
+print(message.isalpha())
+print(message.count("are"))
+print(message.title())
+print(message.replace("doing ","going"))
+
+
+
---
+++ conditions.py
@@ -0,0 +1,52 @@
+# conditional statements
+x = 50
+y = 50
+
+#
+
+
+my_city = "Nairobi"
+# snake casing
+my_favourite_sport = "football"
+
+name = "rodney"
+age = 20
+
+#Types of Data
+#numbers
+height = 3 # integer
+weight = 15.3 # float
+#Strings
+full_name = "Jack kim"
+distance = "45"
+#boolean
+is_listed = True
+is_disabled = False
+
+#conditional statements
+password = "123456"
+db_password = "654321"
+
+if password == db_password:
+    print("Success")
+else:
+    print("Failed")
+
+    score = 65
+    if score > 90:
+        print("A")
+    elif score > 80 and score <= 89:
+        print("A-")
+    elif score >=70 and score <= 79 :
+        print("B+")
+    elif score >=60 and score <= 69:
+        print("C+")
+    elif score >=50 and score <= 50:
+        print("D+")
+    elif score >=40 and score <= 40:
+        print("E+")
+    else:
+        print("F")
+
+
+
